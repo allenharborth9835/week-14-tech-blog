6806e43d145930d6843b4cd03ad53b6f0731d421
Harborth, Allen <allenharborth9835@gmail.com>
2021-09-25 11:23:53 -0500

    add file to create main page structure

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL',
+  onUpdate: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onUpdate: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
