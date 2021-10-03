0b844dfcc8e8182377d97a8e8add6a5e0c34bf7a
Harborth, Allen <allenharborth9835@gmail.com>
2021-10-02 21:00:21 -0500

    fix home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment} = require('../models');
 
 // get all posts for homepage
-router.get('/', checkId ,(req, res) => {
+router.get('/' ,(req, res) => {
   console.log('======================');
   Post.findAll({
     attributes: [
